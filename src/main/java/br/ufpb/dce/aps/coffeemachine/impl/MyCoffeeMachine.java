3f93f09a5d3159fe54f65fc9d716817bbafc9053
Aluno <me@me.com>
2014-07-14 10:20:48 -0300

    Task 1.01 finished for COMPOR

---
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -0,0 +1,16 @@
+package br.ufpb.dce.aps.coffeemachine.impl;
+
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import net.compor.frameworks.jcf.api.ComporFacade;
+
+public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
+
+	public MyCoffeeMachine(ComponentsFactory factory) {
+		// TODO Auto-generated constructor stub
+		factory.getDisplay().info("Insert coins and select a drink!");
+		
+	}
+
+	
+}
