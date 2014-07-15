68064806a683cd6b6ab448081f343d5a37ba7c3c
Aluno <me@me.com>
2014-07-15 09:47:15 -0300

    Task 1.02 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,16 +1,34 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import net.compor.frameworks.jcf.api.ComporFacade;
 
 public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 
+	private int totalcents= 0;
+	private ComponentsFactory Factory;
+	
 	public MyCoffeeMachine(ComponentsFactory factory) {
-		// TODO Auto-generated constructor stub
-		factory.getDisplay().info("Insert coins and select a drink!");
+		this.Factory = factory;
+		this.Factory.getDisplay().info("Insert coins and select a drink!");
 		
 	}
 
+	public void insertCoin(Coin coin) {
+		// TODO Auto-generated method stub
+		 totalcents += coin.getValue();
+		 int inteiro = totalcents/100;
+		 int centavos =totalcents%100;
+		
+		 
+		 this.Factory.getDisplay().info("Total: US$"+" "+ inteiro+"."+ centavos); 
+		
+		
+	}
+	
+	
+		
 	
 }
