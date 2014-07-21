99d923272bd0b82ccd551e6fc6b809161b0a8419
Aluno <me@me.com>
2014-07-21 10:16:37 -0300

    Task 1.02 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,5 +1,9 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import br.ufpb.dce.aps.coffeemachine.CashBox;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
@@ -15,13 +19,15 @@ public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 	private int inteiro;
 	private int centavos;
 	private CashBox cb;
+	private List<Coin> lista;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.centavos=0;
 		this.inteiro=0;
 		this.Factory = factory;
 		this.Factory.getDisplay().info("Insert coins and select a drink!");
-		this.cb = Factory.getCashBox(); 
+		this.cb = Factory.getCashBox();
+		this.lista = new ArrayList<Coin>();
 	}
 
 	public void insertCoin(Coin coin) {
@@ -31,11 +37,8 @@ public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 		}
 		 inteiro += coin.getValue()/100;
 		 centavos += coin.getValue()%100;
-		
-		 
 		 this.Factory.getDisplay().info("Total: US$"+" "+ inteiro+"."+ centavos); 
-		
-		
+		 this.lista.add(coin);
 	}
 
 	public void cancel() {
@@ -45,10 +48,20 @@ public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 		
 	
 		this.Factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
-		cb.release(Coin.halfDollar);
-		this.Factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
-	}
+		
+		for( Coin coin : Coin.reverse()){
+			for(int i=0; i < this.lista.size(); i++){
+				if( this.lista.get(i).equals(coin)){
+					Factory.getCashBox().release(lista.get(i));
+					Coin.reverse();
+				}
+				
+			}
+		}
+			
+			this.Factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 	
+	}
 	
 	
 		
