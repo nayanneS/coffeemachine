40500168b8ec2a600c83af3a2e05ba220d3df79a
Aluno <me@me.com>
2014-07-21 08:56:18 -0300

    Task 1.06 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,9 +1,11 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import br.ufpb.dce.aps.coffeemachine.CashBox;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 import net.compor.frameworks.jcf.api.ComporFacade;
 
 public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
@@ -12,13 +14,14 @@ public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 	private ComponentsFactory Factory;
 	private int inteiro;
 	private int centavos;
+	private CashBox cb;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.centavos=0;
 		this.inteiro=0;
 		this.Factory = factory;
 		this.Factory.getDisplay().info("Insert coins and select a drink!");
-		
+		this.cb = Factory.getCashBox(); 
 	}
 
 	public void insertCoin(Coin coin) {
@@ -36,10 +39,14 @@ public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 	}
 
 	public void cancel() {
-		if(this.inteiro == 0){
+		if(this.inteiro == 0 && this.centavos== 0){
 			throw new CoffeeMachineException("Ausencia de moedas.");
 		}
 		
+	
+		this.Factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+		cb.release(Coin.halfDollar);
+		this.Factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 	}
 	
 	
