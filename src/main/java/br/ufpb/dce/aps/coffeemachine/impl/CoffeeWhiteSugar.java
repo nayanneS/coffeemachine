020d0492174166b35230a8699e28cff7424626df
Aluno <me@me.com>
2014-08-30 10:00:22 -0300

    Task 1.26 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/CoffeeWhiteSugar.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/CoffeeWhiteSugar.java
@@ -29,7 +29,10 @@ public class CoffeeWhiteSugar extends CoffeeBlack  {
 			this.factory.getDisplay().warn(Messages.OUT_OF_CREAMER);
 			return false;
 			}
-			factory.getSugarDispenser().contains(100);
+		if (!this.factory.getSugarDispenser().contains(5)) {
+			this.factory.getDisplay().warn(Messages.OUT_OF_SUGAR);
+			return false;
+			}
 			return true;
 		}
 			
@@ -39,7 +42,7 @@ public class CoffeeWhiteSugar extends CoffeeBlack  {
 		this.factory.getCoffeePowderDispenser().release(15);
 		this.factory.getWaterDispenser().release(80);
 		this.factory.getCreamerDispenser().release(20);;
-		factory.getSugarDispenser().release(100);
+		this.factory.getSugarDispenser().release(5);
 	}
 
 }
